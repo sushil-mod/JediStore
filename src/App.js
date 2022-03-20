e9cfb2a1a2fc8b18efd2776ec49604bf1b4b3993
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2022-03-20 16:30:21 +0530

    Feat: added routes for pages and mockman

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import MockMan from "mockman-js";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
 
 
 function App() {
   return (
    
-    <div>Hello from App</div>
+    <div>
+      <Navbar/>
+        <Routes>
+
+            <Route path="/mockman" element={ <MockMan/> } />
+            <Route path="/" element={ <Home/> }  />
+
+        </Routes>
+      <Footer/>
+    </div>
      
   );
 }

2c16c47a8aa3fd1e7ec1f9557b333d2349bf2af0
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2022-03-23 20:22:04 +0530

    Feat : added links to Route product page

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,37 +1,19 @@
 
-import axios from 'axios';
-import React ,{useState,useEffect} from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom';
+import { useProducts } from '../../context/ProductContext';
+
 
 import './Category.css';
 
 function Category(){
-
-const [category , setCategory ] = useState([]);
-
- useEffect(()=>{
-
-    (async()=>{
-
-        try {
-            const response = await axios.get("/api/categories");
-            console.log(response.data.categories)
-            setCategory(response.data.categories);
-        } catch (error) {
-            console.error(error);
-        }
-
-    })();
-
- },[]);
-
-
+    const { category } = useProducts();
 return<>
     <h1 class="home-content-title margin-btm">Product Catagories</h1>
 
     <div class="home-product flex-center wd-100 margin-btm">
 
-        {category.map((item) => ( <div class="product-link category-div" key={item._id} ><Link  to="/" className='flex-center'> <img src={item.categoryIcons} alt='clothesImage'/> {item.categoryName} </Link></div>  ) )}
+        {category.map((item) => ( <div class="product-link category-div" key={item._id} ><Link  to="/products" className='flex-center'> <img src={item.categoryIcons} alt='clothesImage'/> {item.categoryName} </Link></div>  ) )}
        
     </div>
 </>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,15 +3,18 @@ import React from 'react';
 import Banner from '../../components/banner/Banner';
 import BannerOffer from '../../components/banner/BannerOffer';
 import Category from '../../components/category/Category';
+import Footer from '../../components/footer/Footer';
+import Navbar from '../../components/navbar/Navbar';
 
 
 function Home (){
 
     return <>
-        <div>This is home page </div>
+        <Navbar/>
         <Banner/>
         <Category/>
         <BannerOffer/>
+        <Footer/>
     </>
 
 }

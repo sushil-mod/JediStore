0cb3b728c10bdbd4d9a506d4ee147d2326e9e813
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2022-03-24 11:43:32 +0530

    remove: removed console and comment

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -20,9 +20,6 @@ const ProductProvider = ({ children })=>{
             try {
                
                 const [response1 , response2 ] = await Promise.all( [ axios.get("/api/products") , axios.get("/api/categories") ] ) 
-                // const [response1 , response2 ] = await axios.all( [ axios.get("/api/products") , axios.get("/api/categories") ] );
-                console.log( "response1" ,  response1.data);
-                console.log( "response2" , response2.data);
                 setProductList(response1.data.products);
                 setCategory(response2.data.categories);
                 
